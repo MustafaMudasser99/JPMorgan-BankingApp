79c28e4a9ed8f777797690c9079617fc43a4440e
Mustafa Mudasser <mustafa.ch.mudasser@gmail.com>
2026-04-30 14:02:50 +0100

    Quick fixes for the migrations

--- banking/migrations/0011_chatmessage.py
+++ banking/migrations/0011_chatmessage.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('banking', '0008_userprofile_and_account_choices'),
+        ('banking', '0010_transaction_expires_at_transaction_status'),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
